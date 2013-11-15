8e9f6d0c1daeee0804f926970b839adedb516893
Tumeconnais <Giuseppe06@gmail.com>
2013-11-15 23:40:36 +0100

    test

--- MyPlugin/src/plugin/MyPlugin.java
+++ MyPlugin/src/plugin/MyPlugin.java
@@ -1,8 +1,11 @@
 package plugin;
 
 public class MyPlugin {
-
+ //Class pour test, a export en .Jar :)
 	int x;
+	MyPlugin(){
+		
+	}
 	public void run() {
 		// TODO Auto-generated method stub
 		System.out.println("Hi, I'm MyPlugin");
