2252d6edfaf412cc922189c4d4f69f2abc063c37
Tumeconnais <Giuseppe06@gmail.com>
2013-11-15 13:26:34 +0100

    initial 2

---
+++ MyPlugin/src/plugin/MyPlugin.java
@@ -0,0 +1,13 @@
+package plugin;
+import plugin.Plugin;
+
+
+public class MyPlugin implements Plugin {
+
+	int x;
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("Hi, I'm MyPlugin");
+	}
+
+}
