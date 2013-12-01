2f6a9f63feef24bbd6d03734ca06668cf4902dc0
momogaga <momodu06VL@hotmail.fr>
2013-12-01 15:08:27 +0100

    Plugin epee

--- MyPlugin/src/plugin/Epee.java
+++ MyPlugin/src/plugin/Epee.java
@@ -1,17 +1,20 @@
 package plugin;
 
+import objet.Item;
+import personnage.Personnage;
 import annot.MyItem;
 
+public class Epee {
 
-public class MyPlugin {
- //Class pour test, a export en .Jar :)
-	int x;
-	public MyPlugin(){
+	int force;
+
+	public Epee(int force) {
 		super();
+		this.force = force;
 	}
-	
+
 	@MyItem(icon = "test", nom = "test", type = "test")
-	public void run() {
+	public void equipe(Personnage perso, Item item) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi, I'm MyPlugin");
 	}
